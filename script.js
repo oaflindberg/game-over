a8c816a5a6bdcb642aa582289adeeca1ebda59cf
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2020-05-24 13:10:32 +0200

    Added how to play text and removed obsolete code

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const scoreEl = document.querySelector(".score");
+const scoreDiv = document.querySelector(".score-div");
 const button = document.querySelector(".menu-div button");
 const menu = document.querySelector(".menu-div");
 
 button.addEventListener("click", (e) => {
   menu.classList.add("hidden");
+  scoreDiv.classList.remove("hidden");
 });
