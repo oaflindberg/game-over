e22c2c9fff417a37379086992bdba816a946057c
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2020-05-25 15:53:27 +0200

    Updated README.md for the last time

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const scoreDiv = document.querySelector(".score-div");
 const button = document.querySelector(".menu-div button");
 const menu = document.querySelector(".menu-div");
 
-button.addEventListener("click", (e) => {
+button.addEventListener("click", () => {
   menu.classList.add("hidden");
   scoreDiv.classList.remove("hidden");
 });
